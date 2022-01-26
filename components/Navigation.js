037e085ce631ff3abbfb4320381610cbb067d2ed
Hasan Sefa Ozalp <hasan@hasansefaozalp.com>
2022-01-26 01:00:03 -0800

    '💖'

--- components/Navigation.js
+++ components/Navigation.js
@@ -40,7 +40,6 @@ const pages = [
 
 export default function Navigation() {
   const router = useRouter()
-  console.log(router);
 
   return (
     <Disclosure as="nav" className="bg-white shadow">
@@ -49,7 +48,6 @@ export default function Navigation() {
           <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
             <div className="relative flex justify-between h-16">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                {/* Mobile menu button */}
                 <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
@@ -82,8 +80,6 @@ export default function Navigation() {
 
           <Disclosure.Panel className="sm:hidden">
             <div className="pt-2 pb-4 space-y-1">
-              {/* Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" */}
-
               {pages.map(page => {
                 return (
                   <Disclosure.Button
